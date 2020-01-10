ac4222a08e0d712598b6573c8a2b743613896d50
ScottB <sbakker@maineturnpike.com>
2020-01-10 16:06:34 -0500

    working on JsonRoutines

--- JsonRoutines.cs
+++ JsonRoutines.cs
@@ -235,7 +235,77 @@ namespace DA_JsonLibrary_CS
             // Purpose: Convert a string with escaped characters into control codes
             // Author : Scott Bakker
             // Created: 09/17/2019
-            throw new NotImplementedException();
+            if (value == null)
+            {
+                return null;
+            }
+            if (!value.Contains("\\"))
+            {
+                return value;
+            }
+            StringBuilder result = new StringBuilder();
+            bool lastBackslash = false;
+            int unicodeCharCount = 0;
+            string unicodeValue = "";
+            foreach (char c in value)
+            {
+                if (unicodeCharCount > 0)
+                {
+                    unicodeValue += c;
+                    unicodeCharCount--;
+                    if (unicodeCharCount == 0)
+                    {
+                        result.Append(Convert.ToChar(Convert.ToUInt16(unicodeValue, 16)));
+                        unicodeValue = "";
+                    }
+                }
+                else if (lastBackslash)
+                {
+                    switch (c)
+                    {
+                        case '\"':
+                            result.Append('\"');
+                            break;
+                        case '\\':
+                            result.Append('\\');
+                            break;
+                        case '/':
+                            result.Append('/');
+                            break;
+                        case 'r':
+                            result.Append('\r');
+                            break;
+                        case 'n':
+                            result.Append('\n');
+                            break;
+                        case 't':
+                            result.Append('\t');
+                            break;
+                        case 'b':
+                            result.Append('\b');
+                            break;
+                        case 'f':
+                            result.Append('\f');
+                            break;
+                        case 'u':
+                            unicodeCharCount = 4;
+                            unicodeValue = "";
+                            break;
+                        default:
+                            throw new SystemException($"JSON Error: Unexpected escaped char: {c}");
+                    }
+                    lastBackslash = false;
+                }
+                else if (c == '\\')
+                {
+                    lastBackslash = true;
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
         }
 
         internal static string GetToken(ref int pos, string value)
@@ -245,7 +315,93 @@ namespace DA_JsonLibrary_CS
             // Created: 09/13/2019
             // Notes  : Does not do escaped character expansion here, just passes exact value.
             //        : Properly handles \" within strings properly this way, but nothing else.
-            throw new NotImplementedException();
+            if (value == null)
+            {
+                return null;
+            }
+            char c;
+            // Ignore whitespece before token
+            SkipWhitespace(ref pos, value);
+            // Get first char, check for special symbols
+            c = value[pos];
+            pos++;
+            // Stop if one-character JSON symbol found
+            if (IsJsonSymbol(c))
+            {
+                return c.ToString();
+            }
+            // Have to build token char by char
+            StringBuilder result = new StringBuilder();
+            bool inQuote = false;
+            bool lastBackslash = false;
+            do
+            {
+                // Check for whitespace or symbols to end token
+                if (!inQuote)
+                {
+                    if (IsWhitespace(c))
+                    {
+                        break;
+                    }
+                    if (IsJsonSymbol(c))
+                    {
+                        pos--; // move back one char so symbol can be read next time
+                        break;
+                    }
+                    // Any comments end the token
+                    if (c == '/' && pos < value.Length)
+                    {
+                        if (value[pos] == '*' || value[pos] == '/')
+                        {
+                            pos--; // move back one char so comment can be read next time
+                            break;
+                        }
+                    }
+                    if (c != '\"' && !IsJsonValueChar(c))
+                    {
+                        throw new SystemException($"JSON Error: Unexpected character: {c}");
+                    }
+                }
+                // Check for escaped chars
+                if (inQuote && lastBackslash)
+                {
+                    // Add backslash and character, no expansion here
+                    result.Append('\\');
+                    result.Append(c);
+                    lastBackslash = false;
+                }
+                else if (inQuote && c == '\\')
+                {
+                    // Remember backslash for next loop, but don't add it to result
+                    lastBackslash = true;
+                }
+                else if (c == '\"')
+                {
+                    // Check for quotes around a string
+                    if (inQuote)
+                    {
+                        result.Append('\"'); // add ending quote
+                        break; // Token is done
+                    }
+                    if (result.Length > 0)
+                    {
+                        // Quote in the middle of a token?
+                        throw new SystemException("JSON Error: Unexpected quote char");
+                    }
+                    result.Append('\"'); // add beginning quote
+                    inQuote = true;
+                }
+                else
+                {
+                    // Add this char
+                    result.Append(c);
+                }
+                // Get char for next loop
+                c = value[pos];
+                pos++;
+            }
+            while (pos <= value.Length);
+            return result.ToString();
         }
 
         internal static object JsonValueToObject(string value)
@@ -253,7 +409,53 @@ namespace DA_JsonLibrary_CS
             // Purpose: Convert a string representation of a value to an actual object
             // Author : Scott Bakker
             // Created: 09/13/2019
-            throw new NotImplementedException();
+            if (value == null || value.Length == 0)
+            {
+                return null;
+            }
+            try
+            {
+                if (value.StartsWith("\"", StringComparison.Ordinal) && value.EndsWith("\"", StringComparison.Ordinal))
+                {
+                    value = value.Substring(1, value.Length - 2); // remove quotes
+                    if (DateTime.TryParse(value, out DateTime tempDate))
+                    {
+                        return tempDate;
+                    }
+                    // Parse all escaped sequences to chars
+                    return FromJsonString(value);
+                }
+                if (value == "null")
+                {
+                    return null;
+                }
+                if (value == "true")
+                {
+                    return true;
+                }
+                if (value == "false")
+                {
+                    return false;
+                }
+                // must be numeric
+                if (value.Contains("e") || value.Contains("E"))
+                {
+                    return double.Parse(value);
+                }
+                if (value.Contains("."))
+                {
+                    return decimal.Parse(value);
+                }
+                if (long.Parse(value) > int.MaxValue || long.Parse(value) < int.MinValue)
+                {
+                    return long.Parse(value);
+                }
+                return int.Parse(value);
+            }
+            catch (Exception ex)
+            {
+                throw new SystemException($"JSON Error: Value not recognized: {value}\r\n{ex.Message}");
+            }
         }
 
         internal static void SkipWhitespace(ref int pos, string value)
@@ -263,7 +465,51 @@ namespace DA_JsonLibrary_CS
             // Created: 09/23/2019
             // Notes  : Comments consist of /*...*/ or // to eol (aka line comment)
             //        : An unterminated comment is not an error, it is just all skipped
-            throw new NotImplementedException();
+            if (value == null)
+            {
+                return;
+            }
+            bool inComment = false;
+            bool inLineComment = false;
+            while (pos < value.Length)
+            {
+                if (inComment)
+                {
+                    if (value[pos] == '/' && value[pos - 1] == '*') // found ending "*/"
+                    {
+                        inComment = false;
+                    }
+                    pos++;
+                    continue;
+                }
+                if (inLineComment)
+                {
+                    if (value[pos] == '\r' || value[pos] == '\n') // found end of line
+                    {
+                        inLineComment = false;
+                    }
+                    pos++;
+                    continue;
+                }
+                if (value[pos] == '/' && pos + 1 < value.Length && value[pos + 1] == '*')
+                {
+                    inComment = true;
+                    pos += 3; // must be sure to skip enough so "/*/" pattern doesn't work but "/**/" does
+                    continue;
+                }
+                if (value[pos] == '/' && pos + 1 < value.Length && value[pos + 1] == '/')
+                {
+                    inLineComment = true;
+                    pos += 2; // skip over "//"
+                    continue;
+                }
+                if (IsWhitespace(value[pos]))
+                {
+                    pos++;
+                    continue;
+                }
+                break;
+            }
         }
 
         private static string ToJsonChar(char c)
@@ -271,7 +517,18 @@ namespace DA_JsonLibrary_CS
             // Purpose: Return a character in proper JSON format
             // Author : Scott Bakker
             // Created: 09/13/2019
-            throw new NotImplementedException();
+            if (c == '\\') return "\\\\";
+            if (c == '\"') return "\\\"";
+            if (c == '\r') return "\\r";
+            if (c == '\n') return "\\n";
+            if (c == '\t') return "\\t";
+            if (c == '\b') return "\\b";
+            if (c == '\f') return "\\f";
+            if (c < 32 || c >= 127)
+            {
+                return "\\u" + ((int)c).ToString("x4", null); // always lowercase
+            }
+            return c.ToString();
         }
 
         private static bool IsWhitespace(char c)
@@ -279,7 +536,13 @@ namespace DA_JsonLibrary_CS
             // Purpose: Check for recognized whitespace characters
             // Author : Scott Bakker
             // Created: 09/13/2019
-            throw new NotImplementedException();
+            if (c == ' ') return true;
+            if (c == '\r') return true;
+            if (c == '\n') return true;
+            if (c == '\t') return true;
+            if (c == '\b') return true;
+            if (c == '\f') return true;
+            return false;
         }
 
         private static bool IsJsonSymbol(char c)
@@ -287,7 +550,13 @@ namespace DA_JsonLibrary_CS
             // Purpose: Check for recognized JSON symbol chars which are tokens by themselves
             // Author : Scott Bakker
             // Created: 09/13/2019
-            throw new NotImplementedException();
+            if (c == '{') return true;
+            if (c == '}') return true;
+            if (c == '[') return true;
+            if (c == ']') return true;
+            if (c == ':') return true;
+            if (c == ',') return true;
+            return false;
         }
 
         private static bool IsJsonValueChar(char c)
@@ -295,7 +564,34 @@ namespace DA_JsonLibrary_CS
             // Purpose: Check for any valid characters in a non-string value
             // Author : Scott Bakker
             // Created: 09/23/2019
-            throw new NotImplementedException();
+            switch (c)
+            {
+                case 'n': // null
+                case 'u':
+                case 'l':
+                case 't': // true
+                case 'r':
+                case 'e':
+                case 'f': // false
+                case 'a':
+                case 's':
+                case '0': // numeric
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '-':
+                case '+':
+                case '.':
+                case 'E': // also 'e' checked for above
+                    return true;
+            }
+            return false;
         }
     }
 }
